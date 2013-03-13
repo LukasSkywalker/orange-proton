3a8c43744b27b59922bb124b78b3af29ace03795
Urs Gerber <ug.gerber@gmail.com>
2013-03-13 15:57:02 +0100

    Fix a few require errors and spelling mistakes
    	modified:   app/helpers/database_adapter.rb
    	renamed:    test/unit/base_info_controller_test.rb -> test/unit/base_info_provider_test.rb
    	modified:   thin.pid

--- app/helpers/database_adapter.rb
+++ app/helpers/database_adapter.rb
@@ -1,7 +1,7 @@
 require 'mongo_mapper'
-require_relative '../models/api/doctor'
-require_relative '../models/api/field'
-require_relative '../models/api/icd_entry'
+require_relative '../models/mongo_models/doctor'
+require_relative '../models/mongo_models/field'
+require_relative '../models/mongo_models/icd_entry'
 
 class DatabaseAdapter
   #abstract all database access here
