1050fb65613d5eaf6c84653eaf57f44da62703a5
Urs Gerber <ug.gerber@gmail.com>
2013-03-13 07:42:19 +0100

    Implement MockInfoProvider which returns mocked set of data
    	modified:   app/helpers/information_interface.rb
    	modified:   app/helpers/information_provider.rb
    	modified:   thin.pid

--- app/helpers/information_interface.rb
+++ app/helpers/information_interface.rb
@@ -1,28 +1,25 @@
 require_relative 'information_provider'
 
 module InformationInterface
-
-  @info_provider = MockInfoProvider.new
-
   module Doctors
     def get_close_doctors(field_code, lat, long, count)
-      @info_provider.get_doctors(field_code, lat, long, count)
+      MockInfoProvider.new.get_doctors(field_code, lat, long, count)
     end
   end
 
   module IcdData
     def get_icd_data(field_code, lang)
-      @info_provider.get_icd_data(field_code, lang)
+      MockInfoProvider.new.get_icd_data(field_code, lang)
     end
 
     def get_fields_of_specialization(field_code, max_count, lang)
-      @info_provider.get_fields(field_code, max_count, lang)
+      MockInfoProvider.new.get_fields(field_code, max_count, lang)
     end
   end
 
   module Helpers
     def get_name_of_field(field_code, lang)
-      @info_provider.get_field_name(field_code, lang)
+      MockInfoProvider.new.get_field_name(field_code, lang)
     end
   end
 end
