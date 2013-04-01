37fc468310b84d18e0e98405eb1712ecf77be44c
shaechler <babemusicstar@hotmail.com>
2013-04-02 00:18:07 +0200

    Fix various little UI bugs

--- app/assets/javascripts/request.js
+++ app/assets/javascripts/request.js
@@ -6,8 +6,7 @@
 // -rethink the for-loops. maybe we could simplify them
 
 $(document).ready(function () {
-    I18n.defaultLocale = "de";
-    displayLegend();
+    setLocale($("#lang").val());
     //
     $("#code-name").keyup(function (e) {
         var code = e.which; // normalized across browsers, use this :-)
@@ -18,18 +17,21 @@ $(document).ready(function () {
     });
 
     $("#lang").change(function (e) {
-        mindmapper.sendRequest($("#code-name").val().toUpperCase(), $(this).val());
+        var code =  $("#code-name").val().toUpperCase();
+        if(code !== ""){
+           mindmapper.sendRequest(code, $(this).val());
+        }
         setLocale($(this).val());
     });
 
     /*$("#speciality").click(function (e){
-        mindmapper.sendRequest($("#code-name").val().toUpperCase(), $(this).val());
+     mindmapper.sendRequest($("#code-name").val().toUpperCase(), $(this).val());
 
-    });  */
+     });  */
 
     document.getElementById("code-name").focus();
 
-    if(getUrlVars()["code"] !== undefined){
+    if (getUrlVars()["code"] !== undefined) {
         var code = getUrlVars()["code"].toUpperCase();
         var lang = getUrlVars()["lang"] || "de";
 
@@ -45,7 +47,7 @@ var mindmapper = {
 
     var: response = 0,
 
-    getResponse: function(){
+    getResponse: function () {
         return this.response;
     },
     // This method sends ajax requests to the API
@@ -56,7 +58,7 @@ var mindmapper = {
     },
 
     log: function (text) {
-    // IE does not know the console object
+        // IE does not know the console object
         if (window.console) {
             console.log(text);
         }
@@ -71,7 +73,7 @@ var mindmapper = {
 
         var params = '?code=' + input + '&lang=' + lang;
         var spinner = getSpinner();
-        spinner.spin( document.getElementById('mindmap'))
+        spinner.spin(document.getElementById('mindmap'))
         jQuery.ajax({
             url: '/api/v1/fields/get' + params + '&count=4',
             type: 'GET',
@@ -95,37 +97,37 @@ var mindmapper = {
 
                 var root = $('#mindmap').addRootNode(input + "</br>" + name, {}); // define a root node to attach the other nodes to
                 mm.addNode(root, '<div></div>', {});                              // add a fake node in order to center the root
-                                                                                  // TODO remove mindmap.js and this hack
+                // TODO remove mindmap.js and this hack
 
                 /*###################################*/
                 /*#####   How to use MEGAMIND   #####*/
                 /*###################################*/
 
                 /* Megamind has a concept of different containers where the different nodes are put. This allows us to
-                   split up the page to organize the nodes as we wish. Inside the containers, the nodes are automatically
-                   laid out and distributed. Here are the instructions for generating a new container and adding nodes:
-                   - create an array which holds the nodes. This is pretty straightforward, just see the examples below
-                   - Call the Canvas constructor. The arguments are: new Canvas(left,top,width,height). All are CSS-pixel values
-                   - chain a call to .addNodes(r), specifying the array of nodes. You can add multiple node-types and -sizes in
-                        this array. You can also add click handlers or images or the <cat> element to the nodes.
-                   - chain a call to .doLayout(). This distributes the nodes and makes them fill up the container more or less.
-                   
-                   Notes:
-                   - the doLayout() is not yet complete. The spacing is bad and I should feel bad.
-                   - doLayout() is not deterministic, since it uses some random variables. This is intended.
-                   - You need to add the node-CSS-class to the nodes. Otherwise, they stretch to the entire page's width and
-                        bad things may happen to your eyes, your computer and your grandma.
-                        
-                   You get the picture. Please don't kill hedgehogs.
-                */
+                 split up the page to organize the nodes as we wish. Inside the containers, the nodes are automatically
+                 laid out and distributed. Here are the instructions for generating a new container and adding nodes:
+                 - create an array which holds the nodes. This is pretty straightforward, just see the examples below
+                 - Call the Canvas constructor. The arguments are: new Canvas(left,top,width,height). All are CSS-pixel values
+                 - chain a call to .addNodes(r), specifying the array of nodes. You can add multiple node-types and -sizes in
+                 this array. You can also add click handlers or images or the <cat> element to the nodes.
+                 - chain a call to .doLayout(). This distributes the nodes and makes them fill up the container more or less.
+
+                 Notes:
+                 - the doLayout() is not yet complete. The spacing is bad and I should feel bad.
+                 - doLayout() is not deterministic, since it uses some random variables. This is intended.
+                 - You need to add the node-CSS-class to the nodes. Otherwise, they stretch to the entire page's width and
+                 bad things may happen to your eyes, your computer and your grandma.
+
+                 You get the picture. Please don't kill hedgehogs.
+                 */
 
                 var r = [];
                 var syn = data.synonyms;
                 for (var i = 0; i < Math.min(MAX_SYN, syn.length); i++) {
-                  //mm.addNode(root, '<div class="syn">' + syn[i] + '</div>', {});
-                  $newdiv = $('<div class="syn node ui-draggable">' + syn[i] + '</div>');
-                  $newdiv.appendTo('body');
-                  r.push($newdiv);
+                    //mm.addNode(root, '<div class="syn">' + syn[i] + '</div>', {});
+                    $newdiv = $('<div class="syn node ui-draggable">' + syn[i] + '</div>');
+                    $newdiv.appendTo('body');
+                    r.push($newdiv);
                 }
 
                 var superclass = data.superclass;
@@ -134,8 +136,8 @@ var mindmapper = {
                 $newdiv = $('<div class="super node ui-draggable">' + superclass + '<br />' + super_name + '</div>');
                 $newdiv.appendTo('body');
                 r.push($newdiv);
-                
-                new Canvas(50,50,1000,200).addNodes(r).doLayout();
+
+                new Canvas(50, 50, 1000, 200).addNodes(r).doLayout();
 
                 var p = [];
                 var drgs = data.drgs;
@@ -145,7 +147,7 @@ var mindmapper = {
                     $newdiv.appendTo('body');
                     p.push($newdiv);
                 }
-                new Canvas(50,250,150,200).addNodes(p).doLayout();
+                new Canvas(50, 250, 150, 200).addNodes(p).doLayout();
 
                 var s = [];
                 var exclusiva = data.exclusiva;
@@ -169,18 +171,17 @@ var mindmapper = {
                     var f = fields[i].field;
                     var n = fields[i].name;
                     var r = fields[i].relatedness;
-                    var c = Math.floor((r*156)+100).toString(16); //The more related the brighter
+                    var c = Math.floor((r * 156) + 100).toString(16); //The more related the brighter
                     var color = '#' + c + c + c; //Color is three times c, so it's always grey
                     //mm.addNode(root, '<div class="cat" style="background-color:' + color +'">' + f + ': ' + n + '</div>', {});
                     //$newdiv = $('<div id=speciality class="cat node ui-draggable" style="background-color:' + color +'">' + f + ': ' + n + '</div>');
-                    $newdiv = $('<div class="cat node ui-draggable" style="background-color:' + color +'">'+'' +
-                        '<input type="button" class="cat-button" value="'+f + ': ' + n  +'" onclick=" mindmapper.getDoctors(7.444,46.947,'+ f +');" >' +
-                        + '</div>');
+                    $newdiv = $('<div class="cat node ui-draggable" style="background-color:' + color + '">' + '' +
+                        '<input type="button" class="cat-button" value="' + f + ': ' + n + '" onclick=" mindmapper.getDoctors(7.444,46.947,' + f + ');" >' + +'</div>');
                     $newdiv.appendTo('body');
                     s.push($newdiv);
                 }
-                
-                new Canvas(50,450,1200,200).addNodes(s).doLayout();
+
+                new Canvas(50, 450, 1200, 200).addNodes(s).doLayout();
             },
 
             error: function (xhr, status, error) {
@@ -192,10 +193,10 @@ var mindmapper = {
     //Get the doctors from the db specific to the field and the users location
     //TODO long & lat from interface
     //TODO delete previous Doctor node in Megamind and get Parent note to adjust the Layout
-    getDoctors: function (long,lat,fields) {
+    getDoctors: function (long, lat, fields) {
         var DOC_COUNT = 4;
         jQuery.ajax({
-            url: '/api/v1/docs/get?long='+long+'&lat='+lat+'&field='+fields+'&count='+DOC_COUNT,
+            url: '/api/v1/docs/get?long=' + long + '&lat=' + lat + '&field=' + fields + '&count=' + DOC_COUNT,
             type: 'GET',
             dataType: 'json',
             contentType: "charset=UTF-8",
@@ -208,13 +209,13 @@ var mindmapper = {
                     //TODO add and Format the other Attributes to the Output
                     //mm.addNode(root, '<div class="doctors">' + response[i].name + '</div>', {});
                     $newdiv = $('<div class="doc node ui-draggable">'
-                            + response[i].name +'<br />'
-                            + response[i].address + ' <br />'
-                            + response[i].phone2 + ' <br />' + '</div>');
+                        + response[i].name + '<br />'
+                        + response[i].address + ' <br />'
+                        + response[i].phone2 + ' <br />' + '</div>');
                     $newdiv.appendTo('body');
                     s.push($newdiv);
                 }
-                new Canvas(50,120,500,600).addNodes(s).doLayout();
+                new Canvas(50, 120, 500, 600).addNodes(s).doLayout();
 
             },
             error: function (xhr, status, error) {
@@ -228,17 +229,15 @@ var mindmapper = {
     }
 }
 
-function getGeolocation(){
+function getGeolocation() {
 
 }
 
 // Read a page's GET URL variables and return them as an associative array.
-function getUrlVars()
-{
+function getUrlVars() {
     var vars = [], hash;
     var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-    for(var i = 0; i < hashes.length; i++)
-    {
+    for (var i = 0; i < hashes.length; i++) {
         hash = hashes[i].split('=');
         vars.push(hash[0]);
         vars[hash[0]] = hash[1];
@@ -246,7 +245,7 @@ function getUrlVars()
     return vars;
 }
 
-function getSpinner(){
+function getSpinner() {
     var opts = {
         lines: 13, // The number of lines to draw
         length: 7, // The length of each line
@@ -268,22 +267,20 @@ function getSpinner(){
     return new Spinner(opts);
 }
 
-function setLocale(locale){
+function setLocale(locale) {
     I18n.locale = locale || "de";
     displayLegend();
 }
 
-function displayLegend(){
+function displayLegend() {
 
-   var text = ('<div class="syn legend">'+I18n.t("syn")+'</div>'
-        +'<div class="cat legend">' + I18n.t("cat") +'</div>'
-        +'<div class="doc legend">'+ I18n.t("doc") +'</div>'
-        +'<div class="super legend">' + I18n.t("super")+'</div>'
-        +'<div class="drg legend">'+ I18n.t("drg")+'</div>'
-        +'<div class="exclusiva legend">'+I18n.t("exclusiva")+'</div>'
-        +'<div class="inclusiva legend">'+I18n.t("inclusiva")+'</div>');
-
-
+    var text = ('<div class="syn legend">' + I18n.t("syn") + '</div>'
+        + '<div class="cat legend">' + I18n.t("cat") + '</div>'
+        + '<div class="doc legend">' + I18n.t("doc") + '</div>'
+        + '<div class="super legend">' + I18n.t("super") + '</div>'
+        + '<div class="drg legend">' + I18n.t("drg") + '</div>'
+        + '<div class="exclusiva legend">' + I18n.t("exclusiva") + '</div>'
+        + '<div class="inclusiva legend">' + I18n.t("inclusiva") + '</div>');
 
     document.getElementById("legend").innerHTML = text;
 
