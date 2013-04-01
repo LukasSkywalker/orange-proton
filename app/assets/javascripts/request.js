48bbb4a0b08346d8324b4410806a9bf27dd4a387
shaechler <babemusicstar@hotmail.com>
2013-04-01 21:33:12 +0200

    Solve issue #101 (Display loading spinner)

--- app/assets/javascripts/request.js
+++ app/assets/javascripts/request.js
@@ -67,13 +67,15 @@ var mindmapper = {
         var MAX_EXCLUSIVA = 5;
 
         var params = '?code=' + input + '&lang=' + lang;
-
+        var spinner = getSpinner();
+        spinner.spin( document.getElementById('mindmap'))
         jQuery.ajax({
             url: '/api/v1/fields/get' + params + '&count=4',
             type: 'GET',
             dataType: 'json',
             contentType: "charset=UTF-8",
             success: function (response, status) {
+                spinner.stop();
                 // TODO: we should definitely change the removal routines here. This is US-style. kill everything that moves.
                 // look at mindmap.js's source and try to find the "nodes" array in the window object to remove nodes and stuff from there.
                 History.pushState(null, "OrangeProton", params);
@@ -239,4 +241,26 @@ function getUrlVars()
         vars[hash[0]] = hash[1];
     }
     return vars;
+}
+
+function getSpinner(){
+    var opts = {
+        lines: 13, // The number of lines to draw
+        length: 7, // The length of each line
+        width: 4, // The line thickness
+        radius: 10, // The radius of the inner circle
+        corners: 1, // Corner roundness (0..1)
+        rotate: 0, // The rotation offset
+        color: '#000', // #rgb or #rrggbb
+        speed: 1, // Rounds per second
+        trail: 60, // Afterglow percentage
+        shadow: false, // Whether to render a shadow
+        hwaccel: false, // Whether to use hardware acceleration
+        className: 'spinner', // The CSS class to assign to the spinner
+        zIndex: 2e9, // The z-index (defaults to 2000000000)
+        top: 'auto', // Top position relative to parent in px
+        left: 'auto' // Left position relative to parent in px
+    };
+
+    return new Spinner(opts);
 }
