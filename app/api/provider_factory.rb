606bfd6779358d9e9364b6ca3911d0719f2453c6
Lukas Diener <lukas.diener@hotmail.com>
2013-04-09 10:44:17 +0200

    Use CompoundInfoProvider in development environment. See #125 for a related bug.

--- app/api/provider_factory.rb
+++ app/api/provider_factory.rb
@@ -1,12 +1,10 @@
-require_rel '../models/information_providers'
-
 module ProviderFactory
   class << self
     def get
       case Rails.env
         when 'test'
           MockInfoProvider.new
-        when 'development-remote'
+        when 'development-remote', 'development'
           CompoundInfoProvider.new
         when 'production'
           CompoundInfoProvider.new
