0f8decaf79b253cc46799b2892a66a3f450c63c0
Urs Gerber <ug.gerber@gmail.com>
2013-04-11 16:38:43 +0200

    Fully refactor ThesaurInfoProvider Spec using a stubbed DB adapter
    	modified:   spec/models/thesaur_info_provider_spec.rb

--- spec/models/thesaur_info_provider_spec.rb
+++ spec/models/thesaur_info_provider_spec.rb
@@ -5,16 +5,87 @@ describe ThesaurInfoProvider do
 
   before do
     @provider = ThesaurInfoProvider.new
-    @icd = 'K66.0'  # Peritoneale Adhäsionen
+    @db = @provider.db
+
+    @db.stub(:get_available_thesaur_names).and_return ['chirurgie', 'gynäkologie']
+    @db.stub(:get_fs_codes_for_thesaur_named).with('chirurgie').and_return [1,2]
+    @db.stub(:get_fs_codes_for_thesaur_named).with('gynäkologie').and_return [3,4]
+
+    @db.stub(:get_fs_name).with(1, anything).and_return('Allg. Chirurgie')
+    @db.stub(:get_fs_name).with(2, anything).and_return('Plast. Chirurgie')
+    @db.stub(:get_fs_name).with(3, anything).and_return('Allg. Gynäkologie')
+    @db.stub(:get_fs_name).with(4, anything).and_return('Invasive Gynäkologie')
   end
 
-  it 'should include these specialities' do
-    field1 = FieldEntry.new('Gynäkologische Onkologie', 1, 120)
-    field2 = FieldEntry.new('Frauenkrankheiten und Geburtshilfe (Gynäkologie und Geburtshilfe)', 1, 10)
-    field3 = FieldEntry.new('Schwangerschaftsultraschall', 1, 71)
-    field4 = FieldEntry.new('Reproduktionsmedizin und gynäkologische Endokrinologie', 1, 104)
+  it 'should work only return fields of first thesaur fs codes' do
+    @db.stub(:is_icd_code_in_thesaur_named?).and_return true, false
 
-    var = @provider.get_fields(@icd, 4, 'de')
-    var.should include(field1, field2, field3, field4)
+    @db.should_receive(:get_available_thesaur_names).exactly(:once)
+    @db.should_receive(:is_icd_code_in_thesaur_named?).with('B26.9', 'chirurgie')
+    @db.should_receive(:is_icd_code_in_thesaur_named?).with('B26.9', 'gynäkologie')
+
+    @db.should_receive(:get_fs_name).with(1, 'de').exactly(:once)
+    @db.should_receive(:get_fs_name).with(2, 'de').exactly(:once)
+    @db.should_not_receive(:get_fs_name).with(3, anything)
+    @db.should_not_receive(:get_fs_name).with(4, anything)
+
+    fields = @provider.get_fields('B26.9', 4, 'de')
+
+    fields.should have_at_most(4).items
+    fields.should ==[FieldEntry.new('Allg. Chirurgie', 1, 1), FieldEntry.new('Plast. Chirurgie', 1, 2)]
+  end
+
+  it 'should work only return fields of second thesaur fs codes' do
+    @db.stub(:is_icd_code_in_thesaur_named?).and_return false, true
+
+    @db.should_receive(:get_available_thesaur_names).exactly(:once)
+    @db.should_receive(:is_icd_code_in_thesaur_named?).with('B26.9', 'chirurgie')
+    @db.should_receive(:is_icd_code_in_thesaur_named?).with('B26.9', 'gynäkologie')
+
+    @db.should_receive(:get_fs_name).with(3, 'de').exactly(:once)
+    @db.should_receive(:get_fs_name).with(4, 'de').exactly(:once)
+    @db.should_not_receive(:get_fs_name).with(1, anything)
+    @db.should_not_receive(:get_fs_name).with(2, anything)
+
+    fields = @provider.get_fields('B26.9', 4, 'de')
+
+    fields.should have_at_most(4).items
+    fields.should ==[FieldEntry.new('Allg. Gynäkologie', 1, 3), FieldEntry.new('Invasive Gynäkologie', 1, 4)]
+  end
+
+  it 'should work return all fields of all thesaurs' do
+    @db.stub(:is_icd_code_in_thesaur_named?).and_return true
+
+    @db.should_receive(:get_available_thesaur_names).exactly(:once)
+    @db.should_receive(:is_icd_code_in_thesaur_named?).with('B26.9', 'chirurgie')
+    @db.should_receive(:is_icd_code_in_thesaur_named?).with('B26.9', 'gynäkologie')
+
+    @db.should_receive(:get_fs_name).with(1, 'de').exactly(:once)
+    @db.should_receive(:get_fs_name).with(2, 'de').exactly(:once)
+    @db.should_receive(:get_fs_name).with(3, 'de').exactly(:once)
+    @db.should_receive(:get_fs_name).with(4, 'de').exactly(:once)
+
+    fields = @provider.get_fields('B26.9', 4, 'de')
+
+    fields.should have_at_most(4).items
+    fields.should ==[FieldEntry.new('Allg. Chirurgie', 1, 1),
+                     FieldEntry.new('Plast. Chirurgie', 1, 2),
+                     FieldEntry.new('Allg. Gynäkologie', 1, 3),
+                     FieldEntry.new('Invasive Gynäkologie', 1, 4)]
+
+  end
+
+  it 'should work return empty array if ICD is not in any thesaur' do
+    @db.stub(:is_icd_code_in_thesaur_named?).and_return false
+
+    @db.should_receive(:get_available_thesaur_names).exactly(:once)
+    @db.should_receive(:is_icd_code_in_thesaur_named?).with('B26.9', 'chirurgie')
+    @db.should_receive(:is_icd_code_in_thesaur_named?).with('B26.9', 'gynäkologie')
+
+    @db.should_not_receive(:get_fs_name)
+
+    fields = @provider.get_fields('B26.9', 4, 'de')
+
+    fields.should be_empty
   end
 end
