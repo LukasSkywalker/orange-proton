abde28c1ebf2f105b8f72dd72db05c06d569c24a
Urs Gerber <ug.gerber@gmail.com>
2013-04-23 13:59:53 +0200

    Make tests work again
    	modified:   app/models/database_adapter/database_adapter.rb
    	modified:   app/models/information_providers/components/generic/database_info_provider.rb
    	modified:   app/models/information_providers/components/generic/mock_provider.rb
    	modified:   app/models/information_providers/components/thesaur_info_provider.rb
    	modified:   spec/api/api_spec.rb
    	modified:   spec/models/base_info_provider_spec.rb
    	modified:   spec/models/chop_range_info_provider_spec.rb
    	modified:   spec/models/compound_info_provider_spec.rb
    	deleted:    spec/models/manual_info_provider_spec.rb
    	modified:   spec/models/stringmatch_info_provider_spec.rb
    	modified:   spec/models/thesaur_info_provider_spec.rb

--- spec/models/stringmatch_info_provider_spec.rb
+++ spec/models/stringmatch_info_provider_spec.rb
@@ -5,40 +5,40 @@ describe StringmatchInfoProvider do
 
   before do
     @sampleentry = {
-        "code" => "A00",
-        "text" => "Cholera",
+        'code' => "A00",
+        'text' => "Cholera",
 
     }
     @returned_keywords = [
         {
-        "keyword" => 'bla',
-        "exklusiva" => [],
-        "fmhcodes" => [92]
+        'keyword' => 'bla',
+        'exklusiva' => [],
+        'fmhcodes' => [92]
         }
     ]
     @provider = StringmatchInfoProvider.new
     @db = @provider.db
-    @db.stub(:get_fs_name).with(1, anything).and_return('codologie')
+    @db.stub(:get_fs_name).with(2, anything).and_return('codologie')
 
   end
 
   it 'should recognize keyword' do
-    @sampleentry["text"] = 'This is a TEST'
+    @sampleentry['text'] = 'This is a TEST'
     @db.stub(:get_icd_entry).with(anything, 'de').and_return(@sampleentry)
     @returned_keywords[0]['keyword'] = 'Test'
-    @returned_keywords[0]['fmhcodes'] = [1]
+    @returned_keywords[0]['fmhcodes'] = [2]
     @db.stub(:get_fachgebiete_keywords).and_return(@returned_keywords)
 
     fields = @provider.get_fields('B00',4,'de')
 
-    fields.should ==[FieldEntry.new('codologie', 1, 1)]
+    fields.should ==[FieldEntry.new('codologie', 1, 2)]
   end
 
   it 'should not recognize keyword not contained' do
     @sampleentry['text'] = 'This is a TEST'
     @db.stub(:get_icd_entry).with(anything, 'de').and_return(@sampleentry)
     @returned_keywords[0]['keyword'] = 'Teste'
-    @returned_keywords[0]['fmhcodes'] = [1]
+    @returned_keywords[0]['fmhcodes'] = [2]
     @db.stub(:get_fachgebiete_keywords).and_return(@returned_keywords)
 
     fields = @provider.get_fields('B00',4,'de')
@@ -51,7 +51,7 @@ describe StringmatchInfoProvider do
     @db.stub(:get_icd_entry).with(anything, 'de').and_return(@sampleentry)
     @returned_keywords[0]['keyword'] = 'Test'
     @returned_keywords[0]['exklusiva'] = ['is']
-    @returned_keywords[0]['fmhcodes'] = [1]
+    @returned_keywords[0]['fmhcodes'] = [2]
     @db.stub(:get_fachgebiete_keywords).and_return(@returned_keywords)
 
     fields = @provider.get_fields('B00',4,'de')
--- spec/models/thesaur_info_provider_spec.rb
+++ spec/models/thesaur_info_provider_spec.rb
@@ -8,13 +8,13 @@ describe ThesaurInfoProvider do
     @db = @provider.db
 
     @db.stub(:get_available_thesaur_names).and_return ['chirurgie', 'gynäkologie']
-    @db.stub(:get_fs_codes_for_thesaur_named).with('chirurgie').and_return [1,2]
-    @db.stub(:get_fs_codes_for_thesaur_named).with('gynäkologie').and_return [3,4]
+    @db.stub(:get_fs_codes_for_thesaur_named).with('chirurgie').and_return [2,3]
+    @db.stub(:get_fs_codes_for_thesaur_named).with('gynäkologie').and_return [4,5]
 
-    @db.stub(:get_fs_name).with(1, anything).and_return('Allg. Chirurgie')
-    @db.stub(:get_fs_name).with(2, anything).and_return('Plast. Chirurgie')
-    @db.stub(:get_fs_name).with(3, anything).and_return('Allg. Gynäkologie')
-    @db.stub(:get_fs_name).with(4, anything).and_return('Invasive Gynäkologie')
+    @db.stub(:get_fs_name).with(2, anything).and_return('Allg. Chirurgie')
+    @db.stub(:get_fs_name).with(3, anything).and_return('Plast. Chirurgie')
+    @db.stub(:get_fs_name).with(4, anything).and_return('Allg. Gynäkologie')
+    @db.stub(:get_fs_name).with(5, anything).and_return('Invasive Gynäkologie')
   end
 
   it 'should only return fields of first thesaur fs codes' do
@@ -24,15 +24,15 @@ describe ThesaurInfoProvider do
     @db.should_receive(:is_icd_code_in_thesaur_named?).with('B26.9', 'chirurgie')
     @db.should_receive(:is_icd_code_in_thesaur_named?).with('B26.9', 'gynäkologie')
 
-    @db.should_receive(:get_fs_name).with(1, 'de').exactly(:once)
     @db.should_receive(:get_fs_name).with(2, 'de').exactly(:once)
-    @db.should_not_receive(:get_fs_name).with(3, anything)
+    @db.should_receive(:get_fs_name).with(3, 'de').exactly(:once)
     @db.should_not_receive(:get_fs_name).with(4, anything)
+    @db.should_not_receive(:get_fs_name).with(5, anything)
 
     fields = @provider.get_fields('B26.9', 4, 'de')
 
     fields.should have_at_most(4).items
-    fields.should ==[FieldEntry.new('Allg. Chirurgie', 1, 1), FieldEntry.new('Plast. Chirurgie', 1, 2)]
+    fields.should ==[FieldEntry.new('Allg. Chirurgie', 1, 2), FieldEntry.new('Plast. Chirurgie', 1, 3)]
   end
 
   it 'should only return fields of second thesaur fs codes' do
@@ -42,15 +42,15 @@ describe ThesaurInfoProvider do
     @db.should_receive(:is_icd_code_in_thesaur_named?).with('B26.9', 'chirurgie')
     @db.should_receive(:is_icd_code_in_thesaur_named?).with('B26.9', 'gynäkologie')
 
-    @db.should_receive(:get_fs_name).with(3, 'de').exactly(:once)
     @db.should_receive(:get_fs_name).with(4, 'de').exactly(:once)
-    @db.should_not_receive(:get_fs_name).with(1, anything)
+    @db.should_receive(:get_fs_name).with(5, 'de').exactly(:once)
     @db.should_not_receive(:get_fs_name).with(2, anything)
+    @db.should_not_receive(:get_fs_name).with(3, anything)
 
     fields = @provider.get_fields('B26.9', 4, 'de')
 
     fields.should have_at_most(4).items
-    fields.should ==[FieldEntry.new('Allg. Gynäkologie', 1, 3), FieldEntry.new('Invasive Gynäkologie', 1, 4)]
+    fields.should ==[FieldEntry.new('Allg. Gynäkologie', 1, 4), FieldEntry.new('Invasive Gynäkologie', 1, 5)]
   end
 
   it 'should return all fields of all thesaurs' do
@@ -60,18 +60,18 @@ describe ThesaurInfoProvider do
     @db.should_receive(:is_icd_code_in_thesaur_named?).with('B26.9', 'chirurgie')
     @db.should_receive(:is_icd_code_in_thesaur_named?).with('B26.9', 'gynäkologie')
 
-    @db.should_receive(:get_fs_name).with(1, 'de').exactly(:once)
     @db.should_receive(:get_fs_name).with(2, 'de').exactly(:once)
     @db.should_receive(:get_fs_name).with(3, 'de').exactly(:once)
     @db.should_receive(:get_fs_name).with(4, 'de').exactly(:once)
+    @db.should_receive(:get_fs_name).with(5, 'de').exactly(:once)
 
     fields = @provider.get_fields('B26.9', 4, 'de')
 
     fields.should have_at_most(4).items
-    fields.should ==[FieldEntry.new('Allg. Chirurgie', 1, 1),
-                     FieldEntry.new('Plast. Chirurgie', 1, 2),
-                     FieldEntry.new('Allg. Gynäkologie', 1, 3),
-                     FieldEntry.new('Invasive Gynäkologie', 1, 4)]
+    fields.should ==[FieldEntry.new('Allg. Chirurgie', 1, 2),
+                     FieldEntry.new('Plast. Chirurgie', 1, 3),
+                     FieldEntry.new('Allg. Gynäkologie', 1, 4),
+                     FieldEntry.new('Invasive Gynäkologie', 1, 5)]
 
   end
 
