d2ec04b956efe611bf245b023ecbe5d914ff323f
Gian-Luca Mateo <gianluca.mateo@gmail.com>
2013-05-02 17:25:28 +0200

    further integration of scripts
    	modified:   Rakefile
    	new file:   db/csv_files/chop_keywords.csv
    	new file:   db/csv_files/icd_keywords.csv
    	new file:   db/dictionary_parser/dictionary_parser.rb
    	new file:   db/dictionary_parser/dictionary_parser_runscript.rb
    	modified:   db/script_db_adapter.rb
    	modified:   db/seed.rb

--- db/script_db_adapter.rb
+++ db/script_db_adapter.rb
@@ -4,30 +4,41 @@ include Mongo
 
 class ScriptDBAdapter
   attr_accessor :mongo_client, :db, :coll, :write
-  def initialize (db , collection ,host, port, write, write_pw = '')
+  def initialize (db , collection ,host, port, admin_db, write, write_user = '', write_pw = '')
     mongo_client = MongoClient.new(host, port)
     self.write = write
     if write
-      puts 'Enter your PW for the account pse4_write: '
-      mongo_client.db('admin').authenticate('pse4_write',write_pw)
+      puts "Enter your PW for the account #{write_user}: "
+      mongo_client.db(admin_db).authenticate(write_user,write_pw)
     else
-      mongo_client.db('admin').authenticate('pse4_read','plokij')
+      mongo_client.db(admin_db).authenticate('pse4_read','plokij')
     end
     db = mongo_client.db(db)
     self.coll = db.collection(collection)
   end
 
+  def set_collection (db_name, coll_name)
+    self.db = mongo_client.db(db_name)
+    self.coll = db.collection(coll_name)
+  end
+
   def drop_collection
+    return :no_permission unless self.write
     self.coll.remove()
   end
 
-  def get_docs
-    self.coll.find().to_a
+  def get_docs(doc = {})
+    self.coll.find(doc).to_a
   end
 
   def insert(doc)
     return :no_permission unless self.write
     self.coll.insert(doc)
+  end
+
+  def update_doc(old, new)
+    return :no_permission unless self.write
+    self.coll.update(old, new , { upsert: true })
   end
 
 end
--- db/seed.rb
+++ db/seed.rb
@@ -1,16 +1,23 @@
 require_relative 'script_db_adapter'
+require_relative 'dictionary_parser/dictionary_parser_runscript'
+
+
 
 class Seed
-  def initialize (env)
+  def self.run (env)
     environment = env
 
     db_config = YAML.load_file('config/mongo.yml')[environment]
-    db = db_config['collections']['doctors'][0]
-    coll = db_config['collections']['doctors'][1]
+
+    db = db_config['collections']['icd_keywords'][0]
+    coll = db_config['collections']['icd_keywords'][1]
     host = db_config['host']
     port = db_config['port']
+    admin_db = db_config['database']
+    write_user = 'pse4_write'
+    adapter = ScriptDBAdapter.new(db, coll , host, port, admin_db, false, write_user)
 
-    adapter = ScriptDBAdapter.new(db, coll , host, port, false)
-    puts adapter.get_docs[0]
+
+    DictionaryParserRunscript.run(adapter, "../csv_files/icd_keywords.csv")
   end
 end
